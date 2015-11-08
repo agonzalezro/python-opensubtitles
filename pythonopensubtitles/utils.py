af012056c3578dad2d58474d181ee5692abefd71
Anton Grensjö <anton@grensjo.se>
2015-11-08 19:57:05 +0100

    Added Python 3 compatibility.

--- pythonopensubtitles/utils.py
+++ pythonopensubtitles/utils.py
@@ -38,14 +38,14 @@ class File(object):
         if int(self.size) < 65536 * 2:
             return "SizeError"
 
-        for x in range(65536 / bytesize):
+        for x in range(65536 // bytesize):
             buffer = f.read(bytesize)
             (l_value, ) = struct.unpack(longlongformat, buffer)
             hash += l_value
             hash = hash & 0xFFFFFFFFFFFFFFFF  # to remain as 64bit number
 
         f.seek(max(0, int(self.size) - 65536), 0)
-        for x in range(65536 / bytesize):
+        for x in range(65536 // bytesize):
             buffer = f.read(bytesize)
             (l_value, ) = struct.unpack(longlongformat, buffer)
             hash += l_value
