a80b463bbbfcf0fcc0c9708163a7787c97e7417d
dmcelectrico <dmcelectrico@gmail.com>
2016-10-27 19:19:06 +0200

    Update User-Agent
    OpenSubtitles changed again the User-Agent.
    
    Thanks to @junming for the heads-up and to @itaybia for the solution.

--- pythonopensubtitles/settings.py
+++ pythonopensubtitles/settings.py
@@ -1,4 +1,4 @@
 class Settings(object):
     OPENSUBTITLES_SERVER = 'http://api.opensubtitles.org/xml-rpc'
-    USER_AGENT = 'OSTestUserAgent'
+    USER_AGENT = 'OSTestUserAgentTemp'
     LANGUAGE = 'en'
